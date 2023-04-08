a3db31c426d11349cb2026b26103a8b85051936b
Sébastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2023-04-08 21:04:52 +0200

    docs(sphinx): example file for sphinx config

---
+++ src/leg.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+class Leg:
+    def __init__(self):
+        pass
+
+    def move(self, x, y, z):
+        """
+        Move the leg to the given position
+
+        :param x: x coordinate
+        :param y: y coordinate
+        :param z: z coordinate
+        :return: None
+        """
+        pass
